8a125b52c6231a74f48403c5b4a0eda6bc735124
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-07-06 23:34:16 -0400

    added more practice

--- Exercise Files/Ch04/04_02/finished/react-app/src/App.js
+++ Exercise Files/Ch04/04_02/finished/react-app/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { useState } from "react";
+
+
 
 function App() {
+  const [emotion, setEmotion] = useState("happy");
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
+      <button onClick={() => setEmotion("happy")}>Happy</button>
     </div>
   );
 }
