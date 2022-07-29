7ad0342cbf851f2b6f2dbb4f0d0f6f78193c0874
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-07-28 23:11:28 -0400

    learned more react

--- Exercise Files/Ch05/05_01/start/react-app/src/App.js
+++ Exercise Files/Ch05/05_01/start/react-app/src/App.js
@@ -1,22 +1,21 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useState } from "react";
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const[title, setTitle] = useState("");
+  const[color, setColor] = useState("#000000");
+  const submit = (e) => {
+    e.preventDefault();
+    alert(`${title}, ${color}`);
+    setTitle("");
+    setColor("#000000");
+  };
   return (
-    <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
+    <form onSubmit={submit}>
+      <input value={title} onChange={(event) => setTitle(event.target.value)} type="text" placeholder="color title..." />
+      <input value={color} onChange={(event) => setColor(event.target.value)} type="color" />
+      <button>ADD</button>
+    </form>
   );
 }
 
