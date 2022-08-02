9dbeeb33985a8f157f1707b9728396a8d767d85d
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-08-01 23:03:25 -0400

    added more react hooks

--- Exercise Files/Ch05/05_01/start/react-app/src/App.js
+++ Exercise Files/Ch05/05_01/start/react-app/src/App.js
@@ -1,31 +1,34 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function useInput(initialValue){
-  const [value, setValue] = useState(initialValue);
-  return [
-    {value, onChange: e => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
+function GithubUser({name, location}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+    </div>
+  )
 }
 
 function App() {
-  const[titleProps, resetTitle] = useInput("");
-  const[colorProps, resetColor] = useState("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value}, ${colorProps.value}`
-    );
-    resetTitle();
-    resetColor();
-  };
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://api.github.com/users/moonhighway`)
+    .then(response => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, []);
+
+  if(loading) return <h1>Loading...</h1>;
+  if(error) return <pre>{JSON.stringify(error)}</pre>;
+  if(!data) return null;
+
   return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type="text" placeholder="color title..." />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
+    <GithubUser name={data.name} location={data.location} />
   );
 }
 
